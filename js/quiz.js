b024097f5428a5e207e2b418294a5a838551e3ca
Eric Denton <eric@iamericanartist.com>
2016-06-23 15:30:35 -0500

    CarCards set up in DOM using bootstrap

--- js/quiz.js
+++ js/quiz.js
@@ -2,14 +2,33 @@ console.log("quiz.js loaded");
 
 
 function populatePage (inventory) {
+    var carsToPage = document.getElementById("outputEl");
+    var outPutString = "";
+    var counter = 1;
   // Loop over the inventory and populate the page
-  console.log(">Our Inventory: ",inventory);
+  // console.log("Our Inventory: ",inventory);
+
   for (var i = 0; i < inventory.length; i++) {
-    // inventory[i] +=
-    
+    if (i % 3 === 0) {
+    outPutString += `<div class="row">`
+    }
+    outPutString += `
+      <div class="col-md-4" id="carCard--${counter}"> 
+        <p>${inventory[i].make}</p>
+        <p>${inventory[i].model}</p>
+        <p>${inventory[i].year}</p>
+        <p>${inventory[i].price}</p>
+        <p>${inventory[i].color}</p>
+        <p>${inventory[i].purchased}</p>
+        <p>${inventory[i].description}</p>
+      </div>`;
+        if ((i+1) % 3 === 0 ) {
+        outPutString += `</div>`
+        }
 
+      carsToPage.innerHTML = outPutString;
+      counter++;
   };
-
 
   // Now that the DOM is loaded, establish all the event listeners needed
   // CarLot.activateEvents();
